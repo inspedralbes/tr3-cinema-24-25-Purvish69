842f7f3761215f4d3a45907911ba98034bd32eef
Purvish Patel <a23purpatmah@inspedralbes.cat>
2025-03-05 23:08:08 +0100

    Estructura  para la pagina

--- filmgalaxy/app.vue
+++ filmgalaxy/app.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <Home />
+    <Login />
   </div>
 </template>
+
+<script>
+import Home from './pages/home/index.vue'
+import Login from './pages/login/index.vue'
+
+export default {
+  components: {
+    Home
+  }
+}
+</script>
--- filmgalaxy/nuxt.config.ts
+++ filmgalaxy/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  plugins: [
+    '~/plugins/vuetify.js'
+  ],
+  css: [
+    'vuetify/styles'
+  ],
+
+  modules: [
+    '@pinia/nuxt',
+  ]
 })
